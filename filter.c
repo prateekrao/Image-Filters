bba043e30729379775d1b009fa8056c7a7cd5e37
prateekrao <r_prateek@icloud.com>
2021-06-17 15:09:17 +0530

    Added base files

--- filter.c
+++ filter.c
@@ -9,52 +9,131 @@ int main(int argc, char *argv[])
     // Define allowable filters
     char *filters = "begr";
 
-    // Get filter alphabet and check if it exists
+    // Get filter flag and check validity
     char filter = getopt(argc, argv, filters);
 
     if (filter == '?')
     {
-        printf("Invalid filter.\n");
+        fprintf(stderr, "Invalid filter.\n");
         return 1;
     }
 
-    // Check if user has entered only one filter
+    // Ensure only one filter
     if (getopt(argc, argv, filters) != -1)
     {
-        printf("Only one filter allowed.\n");
+        fprintf(stderr, "Only one filter allowed.\n");
         return 2;
     }
 
     // Ensure proper usage
-    if (argc != 4)
+    if (argc != optind + 2)
     {
-        printf("Usage: filter -[flag] inputfile outputfile\n");
+        fprintf(stderr, "Usage: filter [flag] infile outfile\n");
         return 3;
     }
 
     // Remember filenames
-    char *infile = argv[2];
-    char *outfile = argv[3];
-
+    char *infile = argv[optind];
+    char *outfile = argv[optind + 1];
 
     // Open input file
-    FILE *input = fopen(infile, "r");
-    if (input == NULL)
+    FILE *in = fopen(infile, "r");
+    if (in == NULL)
     {
-        printf("Could not open %s.\n", infile);
+        fprintf(stderr, "Could not open %s.\n", infile);
         return 4;
     }
 
     // Open output file
-    FILE *output = fopen(outfile, "w");
-    if (output == NULL)
+    FILE *out = fopen(outfile, "w");
+    if (out == NULL)
     {
-        fclose(input);
-        printf("Could not create %s.\n", outfile);
+        fclose(in);
+        fprintf(stderr, "Could not create %s.\n", outfile);
         return 5;
     }
 
-    
+    // Read input image BITMAPFILEHEADER
+
+    BITMAPFILEHEADER bf;
+    fread(&bf, sizeof(BITMAPFILEHEADER), 1, in);
 
 
+    // Read input image BITMAPINFOHEADER
+    BITMAPINFOHEADER bi;
+    fread(&bi, sizeof(BITMAPINFOHEADER), 1, in);
+
+    int height = abs(bi.biHeight);
+    int width = bi.biWidth;
+
+    RGBTRIPLE(*image)[width] = calloc(height, width * sizeof(RGBTRIPLE));
+
+    if (image == NULL)
+    {
+        fprintf(stderr, "Not enough memory to store image.\n");
+        fclose(out);
+        fclose(in);
+        return 7;
+    }
+
+    // Iterate over infile's scanlines
+    for (int i = 0; i < height; i++)
+    {
+        // Read row into pixel array
+        fread(image[i], sizeof(RGBTRIPLE), width, in);
+    }
+
+    // Filter image
+    switch (filter)
+    {
+        // Blur
+        case 'b':
+            blur(height, width, image);
+            break;
+
+        // Edges
+        case 'e':
+            edges(height, width, image);
+            break;
+
+        // Grayscale
+        case 'g':
+            grayscale(height, width, image);
+            break;
+
+        // Reflect
+        case 'r':
+            reflect(height, width, image);
+            break;
+
+        //Sepia
+        case 's':
+            sepia(height, width, image);
+            break;
+    }
+
+    //Write the image into the output file
+    fwrite(&bf, sizeof(BITMAPFILEHEADER), 1, out);
+
+    // Write outfile's BITMAPINFOHEADER
+    fwrite(&bi, sizeof(BITMAPINFOHEADER), 1, out);
+
+    // Write new pixels to outfile
+    for (int i = 0; i < height; i++)
+    {
+        // Write row to outfile
+        fwrite(image[i], sizeof(RGBTRIPLE), width, out);
+    }
+
+
+    free(image);
+
+    // Close input image file
+    fclose(in);
+
+    // Close output image file
+    fclose(out);
+
+    return 0;
+}
 
